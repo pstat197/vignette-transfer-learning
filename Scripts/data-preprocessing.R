2ce0d9ad6f7d3fa62ae85b70b7263b1168c9f694
bernie-graves <berniejr01@gmail.com>
2022-11-30 23:31:42 -0800

    worked on model

--- Scripts/data-preprocessing.R
+++ Scripts/data-preprocessing.R
@@ -15,17 +15,49 @@ reddit.data.encoded <- reddit.data %>%
 # train test split
 set.seed(1111)
 
-partition <- reddit.data.encoded %>%
+partition.reddit <- reddit.data.encoded %>%
   initial_split(prop = 0.8)
 
-reddit.train.text <- training(partition) %>%
+reddit.train.text <- training(partition.reddit) %>%
   pull(clean_comment)
 
-reddit.train.labels.encoded <- training(partition) %>%
+reddit.train.labels.encoded <- training(partition.reddit) %>%
   select(-clean_comment)
 
-reddit.test.text <- testing(partition) %>%
+reddit.test.text <- testing(partition.reddit) %>%
   pull(clean_comment)
 
-reddit.test.labels.encoded <- testing(partition) %>%
+reddit.test.labels.encoded <- testing(partition.reddit) %>%
   select(-clean_comment)
+
+
+
+# loading in twitter data
+twitter.data <- read.csv("Data/Twitter_Data.csv")
+twitter.data <- twitter.data[complete.cases(twitter.data),]
+
+# encode data so that each column represents 1 if it that column's label
+# 1 in the neutral column indicates neutral sentiment etc
+twitter.data.encoded <- twitter.data %>% 
+  mutate("Negative" = ifelse(category == -1, 1, 0),
+         "Neutral" = ifelse(category == 0, 1, 0),
+         "Positive" = ifelse(category == 1, 1, 0)) %>%
+  select(-category)
+
+# train test split
+set.seed(1111)
+
+partition.twitter <- twitter.data.encoded %>%
+  initial_split(prop = 0.8)
+
+twitter.train.text <- training(partition.twitter) %>%
+  pull(clean_text)
+
+twitter.train.labels.encoded <- training(partition.twitter) %>%
+  select(-clean_text)
+
+twitter.test.text <- testing(partition.twitter) %>%
+  pull(clean_text)
+
+twitter.test.labels.encoded <- testing(partition.twitter) %>%
+  select(-clean_text)
--- Scripts/model-development.R
+++ Scripts/model-development.R
@@ -7,4 +7,53 @@ library(tfhub)
 # Loading the pre-trained Universal Sentence Embedder
 model.url <- "https://tfhub.dev/google/universal-sentence-encoder/4"
 USE.layer <- tfhub::layer_hub(handle = model.url, trainable = FALSE,
-                              name = "Universal Sentence Embedding Layer")
+                              name = "UniversalSentenceEmbeddingLayer")
+
+# Load in data
+load("Data/Data_Processed.RData")
+
+# train.data <- training(partition)
+# train.text <- train.data %>%
+#   pull(clean_comment)
+# 
+# train.labels.df <- train.data %>%
+#   select(-clean_comment)
+train.labels <- as.matrix(twitter.train.labels.encoded)
+
+# test.data <- testing(partition)
+# test.text <- test.data %>%
+#   pull(clean_comment)
+# test.labels.df <- test.data %>%
+#   select(-clean_comment)
+test.labels <- as.matrix(twitter.test.labels.encoded)
+
+# Building the Model
+model <- keras_model_sequential() %>%
+  USE.layer() %>%
+  layer_reshape(target_shape = c(-1, 512)) %>%
+  bidirectional(layer_lstm(units = 64, return_sequences = T, go_backwards = T)) %>%
+  layer_lstm(units = 64) %>%
+  layer_dense(units = 128) %>%
+  layer_dropout(0.25) %>%
+  layer_dense(3, activation = "softmax")
+
+
+# configure for training
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_adam(),
+  metrics = 'categorical_accuracy'
+)
+
+# train
+history <- model %>%
+  fit(twitter.train.text,
+      train.labels,
+      validation_split = 0.3,
+      epochs = 25)
+
+summary(model)
+
+train.preds <- predict(model, train.text)
+evaluate(model, test.text, test.)
+ 
